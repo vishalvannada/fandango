68bc34a1396d1fe6bb8c90c42b1320b1427bd8bf
vishalvannada <vishal.vannada@sjsu.edu>
2018-04-21 01:45:45 -0700

    Adding App.js and refactoring

--- client/src/index.js
+++ client/src/index.js
@@ -2,22 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import promise from 'redux-promise';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
-import Home from './components/home';
-
-import MovieTime from './components/movietime'
-import AddMovie from './components/addMovie'
-
-import MovieDetail from './components/movieOverview';
-import MovieRating from './components/movieRating';
-import SecretPage from './components/secretPage';
-import CheckOut from './components/payment/checkout';
-import CheckOutPayment from './components/payment/checkoutPayment';
-
-
+import App from './App.js';
 
 
 const createStoreWithMiddleware = applyMiddleware(promise, thunk)(createStore);
@@ -25,21 +13,7 @@ export const store = createStoreWithMiddleware(reducers);
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
-            <div>
-                <Switch>
-                    <Route path="/movietime" component={MovieTime}/>
-                    <Route path="/check-out" component={CheckOut}/>
-                    <Route path="/check-out-payment" component={CheckOutPayment}/>
-                    <Route path="/addmovie" component={AddMovie}/>
-                    <Route path="/movie-overview/:tmdbid" component={MovieDetail}/>
-                    <Route path="/movie-review/:tmdbid" component={MovieRating}/>
-                    <Route path="/secret" component={SecretPage}/>
-                    <Route path="/" component={Home}/>
-
-
-                </Switch>
-            </div>
-        </BrowserRouter>
+        <App/>
     </Provider>
+
     , document.getElementById('root'));
